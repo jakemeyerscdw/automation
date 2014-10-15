0e0157f95c2de1a8d1f0cf8c68cffaa8b97789a5
Brad Pinter <brad.pinter@gmail.com>
2014-10-15 07:24:27 +0000

    cloudauto

--- misc/docker/cloudauto/build.sh
+++ misc/docker/cloudauto/build.sh
@@ -3,5 +3,3 @@ docker build -t pinterb/cloudauto_build .
 docker run -v /var/run/docker.sock:/var/run/docker.sock -v $(which docker):$(which docker) -ti --name cloudauto_build pinterb/cloudauto_build
 docker rm cloudauto_build
 docker rmi pinterb/cloudauto_build
-
-# docker run -it -v /home/docker/projects/github/automation/misc/docker/cloudauto:/cloudauto:rw -v /var/run/docker.sock:/var/run/docker.sock jpetazzo/dind /bin/bash
